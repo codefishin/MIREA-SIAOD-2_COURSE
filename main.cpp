b21542dee42e016ce7202d9e59f13c67eefbca80
stomp <150149808+stomp666@users.noreply.github.com>
2024-09-04 17:43:28 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Practice5.hpp"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // пока что меняется для эксперементации. Позже добавлю меню
+}
