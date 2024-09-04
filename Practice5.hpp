74284ba01da61e3cc0347a1c54712c1feadd10e3
stomp <150149808+stomp666@users.noreply.github.com>
2024-09-04 17:43:59 +0300

    Create Practice5.hpp

---
+++ Practice5.hpp
@@ -0,0 +1,28 @@
+#ifndef __PR_5__H
+#define __PR_5__H
+#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+#include <bitset>
+#include <vector>
+#include <algorithm>
+
+class P1
+{
+public:
+	// класс для 1-ых заданий
+	int Task1(int input);
+	int Task2(int input);
+	void Task3(int input);
+};
+
+// Второй пункт
+
+class P2
+{
+public:
+	// класс для 2-ых заданий
+	int Task1();
+};
+
+#endif
